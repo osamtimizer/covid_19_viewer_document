99f4028ef5f7ac28dbf7a8b8df2185e9520a1fb1
osamtimizer <eaten.jb@gmail.com>
2020-04-12 23:46:32 +0900

    add minimal code

---
+++ pages/privacyPolicy.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const PrivacyPolicy = () => (
+  <Layout>
+    <h1>hello world</h1>
+  </Layout>
+);
+
+export default PrivacyPolicy;
