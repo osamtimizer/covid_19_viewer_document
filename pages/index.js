ed83a16b803822257de08d7273b5286068cd9f4d
osamtimizer <eaten.jb@gmail.com>
2020-04-13 00:50:40 +0900

    add small change2

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ const Index = () => (
     <h1>Covid-19 Viewer</h1>
     <div>
       <h3>このアプリについて</h3>
+      <p>
+        Covid-19 Viewerは、日本のCovid-19に関する情報を閲覧できるアプリです。
+      </p>
     </div>
   </Layout>
 );
