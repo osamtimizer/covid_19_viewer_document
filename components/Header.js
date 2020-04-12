88ddd31d951e6b7605c0cfccf607a5d020a94946
osamtimizer <eaten.jb@gmail.com>
2020-04-13 01:18:22 +0900

    move app title to top

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,15 @@ const linkStyle = {
 const Header = () => {
   return (
     <div>
+      <div>
+        <h1>Covid-19 Viewer</h1>
+      </div>
       <Link href="/">
         <a style={linkStyle}>このアプリについて</a>
       </Link>
       <Link href="/privacyPolicy">
         <a style={linkStyle}>プライバシーポリシー</a>
       </Link>
-      <div>
-        <h1>Covid-19 Viewer</h1>
-      </div>
     </div>
   );
 };
