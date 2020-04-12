77abdb7ebda8b896893a490b259f7f67b50893d7
osamtimizer <eaten.jb@gmail.com>
2020-04-13 00:38:04 +0900

    update PrivacyPolicy

--- pages/privacyPolicy.js
+++ pages/privacyPolicy.js
@@ -3,7 +3,20 @@ import Layout from "../components/Layout";
 
 const PrivacyPolicy = () => (
   <Layout>
-    <h1>hello world</h1>
+    <h1>プライバシーポリシー</h1>
+    <div>
+      <p>
+        本アプリケーションでは、利用状況データを収集するツールとしてFirebase(Google
+        Inc.)を使用しています。
+      </p>
+      <p>
+        そのため、Firebaseが利用者の情報を自動的に取得する場合があります。
+        取得する情報、利用目的、第三者への提供等につきましては、以下のGoogleプライバシーポリシーのリンクをご覧ください。
+      </p>
+      <a href="https://policies.google.com/privacy?hl=ja">
+        Google プライバシーポリシー
+      </a>
+    </div>
   </Layout>
 );
 
