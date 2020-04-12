945dffa1f98ec0ba4b6663305ec9e783028a790c
osamtimizer <eaten.jb@gmail.com>
2020-04-13 00:39:02 +0900

    merge index and about page

--- components/Header.js
+++ components/Header.js
@@ -8,9 +8,6 @@ const Header = () => {
   return (
     <div>
       <Link href="/">
-        <a style={linkStyle}>Home</a>
-      </Link>
-      <Link href="/about">
         <a style={linkStyle}>このアプリについて</a>
       </Link>
       <Link href="/privacyPolicy">
